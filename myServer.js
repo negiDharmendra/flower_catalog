f1cbfb7f3787f338eea939cc514f6997ec94bb7a
Dharmendra Singh <dharmenn@thoughtworks.com>
2015-10-29 11:19:47 +0530

    myServer.js is modified to handle the POST method

--- myServer.js
+++ myServer.js
@@ -1,15 +1,36 @@
 var http = require('http');
 var fs = require('fs');
+var querysString = require("querystring")
 function requestHandler(req,res){
-	var filename = req.url.length>1&&"./"+req.url.substr(1)||"./index.html";
-	var text = fs.readFileSync(filename,"utf-8")
-	// console.log(res.writeHead(200,{'Content-Length': text.length,'Content-Type': 'text/plain' }));
-	// console.log("heelo I'm headers",req.rawHeaders);
-	// console.log("heelo I'm url",req.method);
-	// console.log("heelo I'm clintserver",req.connection.remoteAddress)
-	// console.log("heelo I'm clintserver",req.url)
-	res.write(filename);
-	res.end()
+	var requestData='';
+	var myFiles = [
+		"./abeliophyllum.html", "./ageratum.html", "./guest_book.html", "./index.html",
+		"./abeliophyllum_info.css", "./ageratum_info.css", "./index.css","./guest_book_info.css","./pbase-agerantum.png",
+		"./pbase-abeliophyllum.jpg", "./pbase-agerantum.jpg","./animated-flower-image-0021.gif"
+		]
+	var url = req.url.split('?')[0];
+	var filename = req.url.length>1&&"."+url||"./index.html";
+	if(req.method=='POST'){
+		var name = ''; var comment = '';
+		req.on('data',function(data){
+			requestData+=data;
+			name = querysString.parse(requestData).name;
+			comment = querysString.parse(requestData).comment;
+		});
+		req.on('end',function(){
+			var dataToBeAdded = "<p>"+new Date()+"</p></br>"+"<p>"+name+"</p></br>"+"<p>"+comment+"</p></br></br>";
+			fs.appendFile('./guest_book.html',dataToBeAdded)
+			var text = fs.readFileSync(filename)
+			res.write(text)
+			res.end()
+		})
+	}
+	else if(myFiles.indexOf(filename)!=-1){
+		var text = fs.readFileSync(filename)
+		res.write(text);
+		res.end();
+	}
+	else res.end()
 }
 http.createServer(requestHandler).listen(4000,function(){
 	console.log("Hello I'm listening at port 4000");
