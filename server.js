8e806dd95d07b7ca23796af2846678c811ff0432
Dharmendra Singh <dharmenn@thoughtworks.com>
2015-11-03 19:04:08 +0530

    new server.js is working properly.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs  = require('fs');
+var querystring = require('querystring');
 var EventsEmitter = require('events').EventEmitter;
 var emitter = new EventsEmitter();
 
@@ -7,6 +8,11 @@ var getHandler = [
 	{path:'^/$', handler:serveDefaultFile},
 	{path:'', handler:serveAllFile},
 	{path:'', handler:fileNotFound}
+];
+
+var postHandler = [
+	{path:'', handler:postGivenDataIntoRelevantFile},
+	{path:'', handler:methodNotAllowed}
 ];
 
 var match_URL_patterns = function(url){
@@ -28,7 +34,15 @@ var all_get_handler = function(req,res){
 		emitter.emit('next',get_handler,req,res,next);
 	};
 	next();
-}
+};
+
+var all_post_handler = function(req,res){
+	var post_handler = postHandler.filter(match_URL_patterns(req.url));
+	function next(){
+		emitter.emit('next',post_handler,req,res,next);
+	};
+	next();
+};
 
 var requestHandler = function(req,res){
 	console.log("url==>",req.url)
@@ -40,6 +54,7 @@ var requestHandler = function(req,res){
 		all_get_handler(req,res)
 	}
 }
+//==================================GET=======================================================
 function fileNotFound(req,res,next){
 	res.writeHead(405)
 	console.log(res.statusCode+"\n-------------------------------------------------------");
@@ -61,11 +76,48 @@ function serveAllFile(req,res,next){
 			console.log(res.statusCode+"\n-------------------------------------------------------");
 			res.end(data);
 		}
-		else{
-			next()
-		}
+		else next();
 	})
+};
+//==================================POST=======================================================
+function maintainDataBase(data){
+	var dataBase = fs.existsSync('./data/comment.json')&&JSON.parse(fs.readFileSync('./data/comment.json','utf-8'))||[];
+	dataBase.unshift(data);
+	var jsonData = JSON.stringify(dataBase)
+	fs.writeFileSync('./data/comment.json',jsonData);
+};
+
+function parepareHtml(comment){
+	var time = new Date().toString().substr(0,24)
+	return "<div class='guest_comment'><h>"+time+"</h><div><h>Name:</h>"+
+			"<div id='name'>"+comment.name+"</div></div><div><h>Comment:</h><div id='comment'>"+comment.comment+"</div></div></div>"
 }
+
+function postGivenDataIntoRelevantFile(req,res,next){
+	var data = '';
+	req.on('data',function(chunk){
+		data+=chunk;
+		data = querystring.parse(data);
+	})
+	req.on('end',function(){
+		maintainDataBase(data)
+		var commentData = JSON.parse(fs.readFileSync('./data/comment.json','utf-8'));
+		var dataToBeAdded = commentData.map(parepareHtml)
+		var fileData = fs.readFileSync("./html/guest_comment_iframe.html",'utf-8').split(/\n\r|\n\t|\n/);
+		fileData.splice(26,0,dataToBeAdded.join('\n'))
+		fs.writeFileSync("./html/guest_comment_iframe.html",fileData.join('\n'));
+		res.writeHead(301,{date: new Date(),'content-type': 'text/html',
+		  'content-length': fileData.length,connection: 'close',Location:"../html/guest_book.html"});
+		res.end()
+	})
+};
+
+function methodNotAllowed(req,res,next){
+	res.writeHead(404);
+	console.log(res.statusCode+"\n-------------------------------------------------------");
+	res.end("This"+ req.method+" method is not allowed for this link.")
+}
+
 http.createServer(requestHandler).listen(3000, function(){
 	console.log("Server is started at port \"3000\"")
 });
